5634043152141007dda35a964fe8b100dd4981f0
Turner <turnerp81@gmail.com>
2018-08-29 08:36:25 -0400

    Initial Commit

--- Lab_23/Controllers/HomeController.cs
+++ Lab_23/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Lab_23.Models;
 
 namespace Lab_23.Controllers
 {
@@ -10,6 +11,9 @@ namespace Lab_23.Controllers
     {
         public ActionResult Index()
         {
+            Lab23DB ORM = new Lab23DB();
+            ViewBag.Items = ORM.Items.ToList();
+
             return View();
         }
 
