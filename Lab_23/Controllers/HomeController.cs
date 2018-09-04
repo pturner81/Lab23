81b2778a36a64012136c1bad8b8f538acfb52c6a
Turner <turnerp81@gmail.com>
2018-09-04 01:44:06 -0400

    Added search feature

--- Lab_23/Controllers/HomeController.cs
+++ Lab_23/Controllers/HomeController.cs
@@ -30,5 +30,80 @@ namespace Lab_23.Controllers
 
             return View();
         }
+        public ActionResult NewItem()
+        {
+            return View();
+        }
+        public ActionResult SaveNewItem(Item newItem)
+        {
+            Lab23DB ORM = new Lab23DB();
+
+
+            ORM.Items.Add(newItem);
+
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult SearchItemByName(string Name)
+        {
+            Lab23DB ORM = new Lab23DB();
+
+            ViewBag.Items = ORM.Items.Where(x => x.Name.ToLower().Contains(Name.ToLower())).ToList();
+
+            return View("Index");
+        }
+        public ActionResult DeleteItem(string ID)
+        {
+            Lab23DB ORM = new Lab23DB();
+
+            Item ItemToDelete = ORM.Items.Find(ID);
+
+            ORM.Items.Remove(ItemToDelete);
+
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult ItemDetails(string ID)
+        {
+            Lab23DB ORM = new Lab23DB();
+
+            Item ItemToEdit = ORM.Items.Find(ID);
+
+            if (ItemToEdit == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ItemToEdit = ItemToEdit;
+
+            return View();
+        }
+        public ActionResult SaveChanges(Item UpdatedItem)
+        {
+            Lab23DB ORM = new Lab23DB();
+
+            Item OldRecord = ORM.Items.Find(UpdatedItem.ID);
+
+            //Check for Null
+            if (OldRecord == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            OldRecord.Name = UpdatedItem.Name;
+            OldRecord.Description = UpdatedItem.Description;
+            OldRecord.Quantity = UpdatedItem.Quantity;
+            OldRecord.Price = UpdatedItem.Price;
+
+            //Modifies record (original vs current)
+            ORM.Entry(OldRecord).State = System.Data.Entity.EntityState.Modified;
+
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
     }
 }
